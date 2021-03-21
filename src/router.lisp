d557bc205d4fda22d870b82cee0b1d281c9de366
yanqirenshi <yanqirenshi@gmail.com>
2021-03-21 17:10:08 +0900

    use path-info

--- src/router.lisp
+++ src/router.lisp
@@ -2,17 +2,6 @@
 
 (defvar *router* (make-instance 'ningle:<app>))
 
-(defun val (key params)
-  (cdr (if (stringp key)
-           (assoc key params :test 'string=)
-           (assoc key params))))
-
-;; (setf (ningle:route *router* "/deccots/:id/genius")
-;;       #'(lambda (params)
-;;           (let ((deccot-id (val :id      params))
-;;                 (ensure    (val "ensure" params)))
-;;             (response :body (get-genius-by-deccot-id deccot-id :ensure ensure)))))
-
 (setf (ningle:route *router* "/")
       "Welcome to Genius Party!")
 
